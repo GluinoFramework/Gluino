2eeb36ac1ba5314eda01248b55411a06ba03cb3b
Raxdiam <2185242+RyanGDev@users.noreply.github.com>
2023-11-22 15:24:10 -0500

    Add App class
    - Management of Gluino app and windows
    - Platform utility class

---
+++ src/Gluino/App.cs
@@ -0,0 +1,64 @@
+﻿using Gluino.Native;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Gluino;
+
+public static class App
+{
+    internal static readonly nint NativeInstance;
+    internal static readonly nint AppHInstance;
+
+    static App()
+    {
+        NativeInstance = NativeApp.Create();
+        AppHInstance = NativeLibrary.GetMainProgramHandle();
+    }
+    
+    public static string Name { get; set; } = Assembly.GetEntryAssembly()?.GetName().Name ?? "Gluino";
+    
+    public static Window MainWindow { get; internal set; }
+
+    public static WindowCollection ActiveWindows { get; } = [];
+
+    public static Platform Platform { get; } = new();
+
+    public static void Run(Window mainWindow)
+    {
+        if (MainWindow != null)
+            throw new InvalidOperationException("The application is already running");
+        
+        if (Platform.IsWindows) {
+            SetWin32AppId();
+            NativeApp.Register(NativeInstance, AppHInstance, Name);
+        }
+
+        MainWindow = mainWindow;
+
+        NativeApp.Run(NativeInstance);
+    }
+
+    public static void Exit()
+    {
+        if (NativeInstance == nint.Zero)
+            throw new InvalidOperationException("The application is not running");
+
+        NativeApp.Exit(NativeInstance);
+    }
+
+    private static void SetWin32AppId()
+    {
+        var ass = Assembly.GetEntryAssembly();
+        if (ass == null) return;
+        var companyAttr = ass.GetCustomAttribute<AssemblyCompanyAttribute>();
+        var productAttr = ass.GetCustomAttribute<AssemblyProductAttribute>();
+        var titleAttr = ass.GetCustomAttribute<AssemblyTitleAttribute>();
+        var id = string.Join(".", new[] {
+            companyAttr?.Company,
+            productAttr?.Product,
+            titleAttr?.Title
+        }.Where(x => x != null));
+
+        NativeApp.SetCurrentProcessExplicitAppUserModelID(string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id) ? Name : id);
+    }
+}
---
+++ src/Gluino/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gluino;
+
+public class Platform
+{
+    internal Platform() { }
+
+    public OSPlatform OS { get; } =
+        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OSPlatform.Windows :
+        RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSPlatform.Linux :
+        RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OSPlatform.OSX :
+        throw new PlatformNotSupportedException();
+
+    public Architecture Arch { get; } = RuntimeInformation.ProcessArchitecture;
+
+    public static bool IsWindows { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+    public static bool IsLinux { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    public static bool IsOSX { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+}
